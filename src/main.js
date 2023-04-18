7814df532dd367ba9c6e5f9452e13e6937d39fac
jacob-d242 <jacobmoracha@gmail.com>
2023-04-18 23:55:26 +0300

    starting out

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// import { ApolloClient, InMemoryCache } from 'apollo-boost'
 
+// const client = new ApolloClient ({
+//     uri : 'https://att-backend.herokuapp.com/',
+//     cache : new InMemoryCache
+// })
 createApp(App).mount('#app')
