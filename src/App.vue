7c7790a8a615ca2fbd497fc8abe6f38eb65294f3
jacob-d242 <jacobmoracha@gmail.com>
2023-04-19 18:26:55 +0300

    created hompepage

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,14 @@
   <div class="bg-white w-full">
       <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div class="text-center">
-          <h4 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Data to enrich your online business</h4>
-          <p class="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+          <p class="text-xl font-bold tracking-tight text-gray-900 ">
+            Streamline your school's administrative tasks with our management system.
+             Our platform is packed with features designed to save you time and increase productivity. 
+             Get started today and experience the benefits of a truly modern school management system.
+          </p>
+          <p class="mt-6 text-lg leading-8 text-gray-600">
+            Sign up today to experience the power of our school management system. 
+          </p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
             <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm
              hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -15,5 +21,5 @@
 </template>
 
 <script >
-import { ref } from 'vue'
+
 </script>
